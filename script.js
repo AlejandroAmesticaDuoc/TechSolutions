98d42e21b01fe3a7fd25074201a22febcc5bfeed
AlejandroAmesticaDuoc <alej.amestica@duocuc.cl>
2025-10-06 21:09:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const users = [
-  { name: 'Victor', age: 26 , email: 'Techsolutions@gmail.com' }
+  { name: 'Victor', age: 26, email: 'Techsolutions@gmail.com' }
 ];
 
-    function showUsers() {
-      const list = document.getElementById('userList');
-      }
+function showUsers() {
+  const list = document.getElementById('userList');
 
-      list.innerHTML = users.map(u => `
-        <li>
-          <div class="user-name">${u.name}</div>
-          <div class="user-email">📧 ${u.email}</div>
-          <div class="user-age">🎂 Edad: ${u.age}</div>
-        </li>
-      `).join('');
-    }
+  list.innerHTML = users.map(u => `
+    <li>
+      <div class="user-name">👤 ${u.name}</div>
+      <div class="user-email">📧 ${u.email}</div>
+      <div class="user-age">🎂 Edad: ${u.age}</div>
+    </li>
+  `).join('');
+}
 
-    showUsers();
+showUsers();
