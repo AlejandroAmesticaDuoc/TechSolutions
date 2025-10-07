c7d3b4560934c36e10fc15668b5af948b808402a
AlejandroAmesticaDuoc <alej.amestica@duocuc.cl>
2025-10-06 21:03:00 -0300

    Implement showUsers function to display user
    Fix syntax errors and implement showUsers function to display user information.

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ const users = [
   { name: 'Victor', age: 26 , email: 'Techsolutions@gmail.com }
 ];
 
-showUsers()
+    function showUsers() {
+      const list = document.getElementById('userList');
+      }
+
+      list.innerHTML = users.map(u => `
+        <li>
+          <div class="user-name">${u.name}</div>
+          <div class="user-email">📧 ${u.email}</div>
+          <div class="user-age">🎂 Edad: ${u.age}</div>
+        </li>
+      `).join('');
+    }
+
+    showUsers();
